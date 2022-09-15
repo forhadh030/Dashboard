4ffd487c72af3dc7b84f455d039e9174be6ad191
Forhad Hussain <forhad@ymail.com>
2022-09-15 08:35:45 -0700

    update

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './about.css'
+import HeaderImage from '../../images/header_bg_1.jpg'
+import Header from '../../components/Header'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <Header title="About Us" image={HeaderImage}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Veniam eius neque maiores delectus officia ipsa maxime facilis 
+        nobis, cupiditate aliquid quo aperiam voluptas non tenetur libero 
+        qui architecto distinctio similique!
+      </Header>
+    </>
   )
 }
 
