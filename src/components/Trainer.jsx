c250ebb53851468c0829e9bf84567d179bd6e8d9
Forhad Hussain <forhad@ymail.com>
2022-09-22 16:41:30 -0700

    update

---
+++ src/components/Trainer.jsx
@@ -0,0 +1,24 @@
+import Card from '../UI/Card'
+
+const Trainer = ({image, name, job, socials}) => {
+  return (
+    <div>
+      <Card className={"trainer"}>
+        <div className="trainer_img">
+            <img src={image} alt={name} />
+        </div>
+        <h3>{name}</h3>
+        <p>{job}</p>
+        <div className="trainer_socials">
+            {
+                socials.map(({icon, link}, index) => {
+                    return <a key={index} href={link} target="_blank" rel="noreffer noopener">{icon}</a>
+                })
+            }
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default Trainer
--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_5.jpg'
+import {trainers} from '../../data'
+import {BsInstagram} from 'react-icon/bs'
+import {AiOutlineTwitter} from 'react-icon/ai'
+import {FaFacebookF, FaLinkedIn} from 'react-icon/fa'
 import './trainers.css'
+import Trainer from '../../components/Trainer'
 
 const Trainers = () => {
   return (
-    <div>Trainers</div>
+    <>
+    <Header title="Our Trainers" image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero dolorum quae maxime quaerat culpa porro amet dignissimos minima ducimus, doloribus enim vitae molestias dolores ad repellat impedit magnam! Praesentium.
+    </Header>
+    <section className="trainers">
+      <div className="container trainers_container">
+        {
+          trainers.map(({id, image, name, job, socials}) => {
+            return <Trainer key={id} image={image} name={name} job={job} socials={ 
+            [
+              {icon: <BsInstagram/>, link: socials[0]},
+              {icon: <AiOutlineTwitter/>, link: socials[1]},
+              {icon: <FaFacebookF/>, link: socials[2]},
+              {icon: <FaLinkedIn/>, link: socials[3]}
+            ]
+            }/>
+          })
+        }
+      </div>
+    </section>
+    </>
   )
 }
 
