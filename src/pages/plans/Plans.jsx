7ad6162532d2402751ae82e387613ca1cb3e97ab
Forhad Hussain <forhad@ymail.com>
2022-09-27 20:22:32 -0700

    update

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_4.jpg'
 import Card from '../../UI/Card'
-import {plans} from '../../data'
+import { plans } from '../../data'
 import './plans.css'
 
 const Plans = () => {
