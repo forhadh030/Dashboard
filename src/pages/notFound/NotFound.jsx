874f4b17d8522fb8fafe7af00fa085b11b42cf4b
Syed Hussain <syedf.hussain7@gmail.com>
2022-10-26 17:30:17 -0700

    update

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-dom'
+import { Link } from 'react-router-dom'
 import './notFound.css'
 
 const NotFound = () => {
--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,9 +1,9 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_5.jpg'
 import { trainers } from '../../data'
-import { BsInstagram } from 'react-icon/bs'
-import { AiOutlineTwitter } from 'react-icon/ai'
-import { FaFacebookF, FaLinkedIn } from 'react-icon/fa'
+import { BsInstagram } from 'react-icons/bs'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { FaFacebookF, FaLinkedin } from 'react-icons/fa'
 import './trainers.css'
 import Trainer from '../../components/Trainer'
 
@@ -19,10 +19,10 @@ const Trainers = () => {
           trainers.map(({id, image, name, job, socials}) => {
             return <Trainer key={id} image={image} name={name} job={job} socials={ 
             [
-              {icon: <BsInstagram/>, link: socials[0]},
-              {icon: <AiOutlineTwitter/>, link: socials[1]},
-              {icon: <FaFacebookF/>, link: socials[2]},
-              {icon: <FaLinkedIn/>, link: socials[3]}
+              {icon: <BsInstagram />, link: socials[0]},
+              {icon: <AiOutlineTwitter />, link: socials[1]},
+              {icon: <FaFacebookF />, link: socials[2]},
+              {icon: <FaLinkedin />, link: socials[3]}
             ]
             }/>
           })
